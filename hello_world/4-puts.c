34eaca26e3c68af008b57205fbd6f9dfd574837b
michaellgans <michaellgans@hotmail.com>
2023-05-24 11:08:35 -0700

    Betty I love you

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
+#include "main.h"
 
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char str1[55];
+
+	strcpy(str1, "\"Programming is like building a multilingual puzzle\n");
+	puts(str1);
+	return (0);
+}
