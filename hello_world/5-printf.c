c389df48648dfbeafcba6ebcb30932875331c372
michaellgans <michaellgans@hotmail.com>
2023-05-24 11:40:46 -0700

    she wanted a comment

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	printf("Text goes here");
+	printf("withproper grammar, but the outcome is a piece of art,");
 	return (0);
 }
