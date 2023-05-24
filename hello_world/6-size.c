560c83dda3204f9fe467c4f8ecaabb7d3684a490
michaellgans <michaellgans@hotmail.com>
2023-05-24 12:22:53 -0700

    removed .

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	double d;
 	float f;
 
-	printf("Size of a char: %lu byte(s).\n", (unsigned long)sizeof(c));
-	printf("Size of a int: %lu byte(s).\n", (unsigned long)sizeof(i));
-	printf("Size of a long int %lu byte(s).\n", (unsigned long)sizeof(l));
-	printf("Size of a long long int: %lu byte(s).\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu byte(s).\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
