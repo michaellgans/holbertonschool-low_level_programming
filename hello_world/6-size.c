9608581ce362ff0ddace0bebebe2503dced2ed68
michaellgans <michaellgans@hotmail.com>
2023-05-24 12:19:26 -0700

    removed spaces

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	double d;
 	float f;
 
-	printf("Size of a char: %lu (s).\n", (unsigned long)sizeof(c));
-	printf("Size of a int: %lu (s).\n", (unsigned long)sizeof(i));
-	printf("Size of a long int  %lu (s).\n", (unsigned long)sizeof(l));
-	printf("Size of a long long int: %lu (s).\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu (s).\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu(s).\n", (unsigned long)sizeof(c));
+	printf("Size of a int: %lu(s).\n", (unsigned long)sizeof(i));
+	printf("Size of a long int %lu(s).\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu(s).\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu(s).\n", (unsigned long)sizeof(f));
 	return (0);
 }
