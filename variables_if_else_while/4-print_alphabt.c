94cf0ff6d9b139d38a05d8cbb924f4ef614e3bc1
michaellgans <michaellgans@hotmail.com>
2023-05-25 13:05:13 -0700

    if inside the for loop

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -13,10 +13,10 @@ int main(void)
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
-	}
-	for (c = 'A'; c <= 'Z'; c++)
-	{
-		putchar(c);
+
+		if (c == 'e' || c == 'q')
+		continue;
+
 	}
 	putchar('\n');
 
