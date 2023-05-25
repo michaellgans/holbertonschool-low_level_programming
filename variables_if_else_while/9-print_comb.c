ab7a0569f54891cf7d0d36c53ad0200072572d09
michaellgans <michaellgans@hotmail.com>
2023-05-25 13:56:19 -0700

    space?

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+		putchar(, );
+	}
+	putchar('\n');
+
+	return (0);
+}
