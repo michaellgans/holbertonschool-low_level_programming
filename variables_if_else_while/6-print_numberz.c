e92775874e0db278abaed3dffa4ee03e7bb0168c
michaellgans <michaellgans@hotmail.com>
2023-05-25 13:23:05 -0700

    not sure if I can use char

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
