7ec848096fd9bae06eb96b1af20bf11fd5a1f915
michaellgans <michaellgans@hotmail.com>
2023-05-25 08:58:27 -0700

    Ran Betty and GCC

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+
+        if()
+        {
+        printf("Last digit of $d is ", n)
+        }
+        elif(last digit of n is > 5)
+        {
+        printf("Last digit of $d is $d and is greater than 5", n)
+        }
+        elif(last digit of n is = 0)
+        {
+        printf("Last digit of $d is $d and is 0", n)
+        }
+        elif(last digit of n is < 6 != 0)
+        {
+        printf("Last digit of $d is $d and is less than 6 and not 0", n)
+        }
+
+        return (0);
+}
+
+
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        printf(with proper grammar, but the outcome is a piece of art,n);
+        return (0);
+}
