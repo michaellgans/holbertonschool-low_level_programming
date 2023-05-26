0a73da3f99cbe996cb94f84e3fc161fa9787edc8
michaellgans <michaellgans@hotmail.com>
2023-05-26 12:47:08 -0700

    thanks sarah

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code
+ * main - entry point thank you Sarah
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char c[] = "_putchar";
+	int x;
+
+	for (x = 0; x < 7; x++)
+	{
+		_putchar(c[x]);
+	}
+	_putchar('\n');
+
 	return (0);
 }
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+/*This is the document for all the prototypes*/
+
+int _putchar(char c);
+void print_alphabet(void);
+
+#endif
