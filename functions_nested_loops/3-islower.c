3116db76b919d7165ac6451ab0548a73a8413447
michaellgans <michaellgans@hotmail.com>
2023-05-26 19:22:19 -0700

    MAAAAYBE?

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _islower - DONE
+ *
+ * @c: DONE
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (0);
+}
