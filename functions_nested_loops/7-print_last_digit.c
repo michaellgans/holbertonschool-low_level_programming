45ffdb20bab065cfd6d5ad6b814a68be6d151b17
michaellgans <michaellgans@hotmail.com>
2023-05-29 09:12:11 -0700

    I think I got it

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - CHECK YOUR PROTOTYPE
+ * @x: CHECK YOUR VAR
+ * Return: CHECK YOUR PROTOTYPE
+ */
+int print_last_digit(int x)
+{
+	x = x % 10;
+	return (x);
+}
