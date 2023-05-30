9456c6c50016425e71e80e6a1f3077eb088f753a
michaellgans <michaellgans@hotmail.com>
2023-05-30 12:28:30 -0700

    .

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * times_table - DONE
+ */
+void times_table(void)
+{
+	int x;
+
+	char c[] = "0,  0,  0,  0,  0,  0,  0,  0,  0,  0";
+
+	for (x = 0; x < 38; x++)
+	{
+		_putchar(c[x]);
+	}
+	_putchar('\n');
+}
