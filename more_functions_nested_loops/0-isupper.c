4bee506e3dcdec9baa320eae649e964f6c0c4bb0
michaellgans <michaellgans@hotmail.com>
2023-05-31 16:27:39 -0700

    0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - entry point
+ * @c: variable
+ * Return: int
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 65)
+	{
+		return (1);
+	}
+return (0);
+}
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+# holbertonschool-low_level_programming
+
+FIRST C REPOSITORY!!
+
+Resources:
+https://github.com/hs-hq/Betty/wiki  <-- BETTY RULES
