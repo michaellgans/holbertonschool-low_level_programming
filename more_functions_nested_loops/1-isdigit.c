f501f24417262c67b254b5ed6398317422fb217d
michaellgans <michaellgans@hotmail.com>
2023-05-31 16:42:15 -0700

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - entry point
+ * @c: this is the variable
+ * Return: 
+ */
+
+int _isdigit(int c);
+
+{
+	if (c <= 48 && c >= 57)
+	{
+		return (1);
+	}
+return (0);
+}
