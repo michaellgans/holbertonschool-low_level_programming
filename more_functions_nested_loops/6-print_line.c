5d27a8aa314a35e5df83c70d5a1b052f10d08be1
michaellgans <michaellgans@hotmail.com>
2023-06-01 07:00:44 -0700

    might need a while loop

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: number of spaces in the line
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x <= n; x++)
+		putchar('_');
+	}
+	putchar('\n');
+}
