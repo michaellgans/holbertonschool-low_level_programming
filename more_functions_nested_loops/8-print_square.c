d69fc494d687150f956750e3f2bf7980e6050cca
michaellgans <michaellgans@hotmail.com>
2023-06-01 09:50:16 -0700

    I think I did it.......

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size of the sqare
+ */
+
+void print_square(int size)
+{
+	int rows;
+	int columns;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	for (rows = 0; rows < size; rows++)
+	{
+		for (columns = 0; columns < size; columns++)
+		{
+			putchar(35);
+		}
+	putchar(35);
+	putchar('\n');
+	}
+}
