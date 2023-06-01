e653bb9e5ffae835d54c1a91274d8a1a6a36b11a
michaellgans <michaellgans@hotmail.com>
2023-06-01 10:46:52 -0700

    got it

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 1;
+
+	for (x = 1; x < 101; x++)
+	{
+		if (x % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		else if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("Fizz Buzz ");
+			continue;
+		}
+	printf("%i ", x);
+	}
+	putchar('\n');
+	return (0);
+}
