d1039d56be143495372b41250ac85ff1bc4ac0bb
michaellgans <michaellgans@hotmail.com>
2023-06-01 07:08:34 -0700

    ASCII??

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,13 +9,13 @@ void print_line(int n)
 {
 	int x;
 
-	if (n <= 0)
+	if (n <= 48)
 	{
 		putchar('\n');
 	}
 	else
 	{
-		for (x = 0; x <= n; x++)
+		for (x = 48; x <= n; x++)
 		putchar('_');
 	}
 	putchar('\n');
