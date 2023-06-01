c7cb14e18a9e40955baa377bb4e8956e292938d4
michaellgans <michaellgans@hotmail.com>
2023-06-01 09:01:19 -0700

    progress

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal lines hopefully
+ * @n: how many lines it needs to be
+ */
+
+void print_diagonal(int n)
+{
+	int lines;
+	int spaces;
+
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (spaces = 0; spaces < n; spaces++)
+		{
+			for (lines = 0; lines < n; lines++)
+			{
+				putchar(' ');
+			}
+		putchar(92);
+		putchar('\n');
+		}
+	}
+}
