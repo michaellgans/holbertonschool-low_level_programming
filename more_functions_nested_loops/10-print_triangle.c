9617ed13fdbc127af81ec9a885b0cee16ea86819
michaellgans <michaellgans@hotmail.com>
2023-06-02 07:15:18 -0700

    ?

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,23 +10,26 @@ void print_triangle(int size)
 	int spaces;
 	int rows;
 
-	if (size <= 0)
+	if (size > 0)
+	{
+		for (spaces = 0; spaces < size; spaces++)
+		{
+			for (rows = 0; rows < size; rows++)
+			{
+				if ((spaces + rows) > size)
+				{
+					putchar(35);
+				}
+				else
+				{
+					putchar(' ');
+				}
+			}
+			putchar('\n');
+		}
+	}
+	else
 	{
 		putchar('\n');
-	}
-	for (spaces = 0; spaces < size; spaces++)
-	{
-		for (rows = 0; rows < size; rows++)
-		{
-			if ((spaces + rows) > size)
-			{
-				putchar(35);
-			}
-			else
-			{
-				putchar(' ');
-			}
-		}
-	putchar('\n');
 	}
 }
