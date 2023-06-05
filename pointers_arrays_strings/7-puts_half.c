f8c684a91ed1f3c57bfdc425bdac47468dea6306
michaellgans <michaellgans@hotmail.com>
2023-06-05 10:08:24 -0700

    I think I got it in one

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - function
+ * @str: input
+ */
+
+void puts_half(char *str)
+{
+	int x;
+	int length = _strlen(str);
+
+	for (x = 0; x < length; x++)
+	{
+		if (x < (length / 2))
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(str[x]);
+		}
+	}
+	_putchar('\n');
+}
+
