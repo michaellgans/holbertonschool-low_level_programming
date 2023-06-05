ee354af2237fbca8e82d758eaa015eb2d8bdb1e9
michaellgans <michaellgans@hotmail.com>
2023-06-05 14:57:39 -0700

    testing for ascii

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _atoi - function
+ * @s: var
+ * Return: 0
+ */
+
+int _atoi(char *s)
+{
+	int x;
+	int length = _strlen(s);
+
+	for (x = 0; x < length; x++)
+	{
+		if (x >= 48 && x <= 57)
+		{
+			printf("%i\n", x);
+		}
+	}
+	return (0);
+}
