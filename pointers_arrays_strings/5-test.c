2ca30175f6275258d6e7207af965f6fd38a6b955
michaellgans <michaellgans@hotmail.com>
2023-06-06 09:42:16 -0700

    NOTES

---
+++ pointers_arrays_strings/5-test.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - function
+ * @s: This is a variable :)
+ */
+
+void rev_string(char *s)
+{
+	int x;
+	int length = _strlen(s);
+	int temp;
+
+	for (x = 0; x < length / 2; x++)
+	{
+		temp = s[x];
+		s[x] = s[length - x - 1];
+		s[length - x - 1] = temp;
+	}
+}
