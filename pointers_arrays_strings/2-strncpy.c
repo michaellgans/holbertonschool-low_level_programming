c5d06794f80d4919044c8a176d8e402691c63577
michaellgans <michaellgans@hotmail.com>
2023-06-09 09:10:41 -0700

    .

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strncpy - function that coppies strings
+ * @dest: new string
+ * @src: old string
+ * @n: length to change
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+	int y;
+
+	for (x = 0, y = 0; x < n && src[y] != '\0'; x++, y++)
+	{
+		dest[x] = src[y];
+	}
+	return (dest);
+}
