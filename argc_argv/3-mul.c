7d6d1f9728e316cc4d0988a8038eff5acbb7c518
michaellgans <michaellgans@hotmail.com>
2023-06-19 19:25:34 -0700

    m

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -17,19 +17,17 @@ int main(int argc, char *argv[])
 
 	(void)argc;
 
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
-	product = x * y;
-
-	if (argc > 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	product = x * y;
+
 	printf("%i\n", product);
-	}
 
 	return (0);
 }
--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -6,3 +6,33 @@
  * @argc: things
  * Return: 0
  */
+
+int main (int argc, char *argv[])
+{
+	int x = 0;
+	int y = 0;
+	int product;
+
+	(void)argc;
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	product = x + y;
+	
+	if (argc = 0)
+	{
+		printf("0\n");
+	}
+	else if
+	if (argc < 2 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%i\n", product);
+	}
+
+	return (0);
+}
