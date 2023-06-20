5f59f2bb2997968c6679ca32231386483337ae37
michaellgans <michaellgans@hotmail.com>
2023-06-20 16:44:37 -0700

    m

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,35 +1,40 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * main - entry point
  * @argc: stuff
  * @argv: things
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-	int x = 0;
-	int y = 0;
-	int product;
+	int x;
+	unsigned int y, result = 0;
+	char *ptr;
 
-	if (argc == 0)
+	if (argc > 1)
+	{
+		for (x = 1; x < argc; x++)
+		{
+			ptr = argv[x];
+			for (y = 0; y < strlen(ptr); y++)
+			{
+				if (ptr[y] < 48 || ptr[y] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			result += atoi(ptr);
+			ptr++;
+		}
+		printf("%i\n", result);
+	}
+	else
 	{
 		printf("0\n");
 	}
-	else if (argc < 48 || argc > 57)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
-	product = x + y;
-
-	printf("%i\n", product);
-
 	return (0);
 }
