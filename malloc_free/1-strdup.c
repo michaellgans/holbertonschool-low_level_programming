82da788d9620d00e406c8a7e06fcc1014cce86ec
michaellgans <michaellgans@hotmail.com>
2023-06-28 10:13:26 -0700

    used strlen, but may need to use mine

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,22 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * strdup - returns pointer to a copy of a string
+ * _strdup - returns pointer to a copy of a string
  * @str: pointer of origional string
  * Return: char
  */
 
 char *_strdup(char *str)
 {
-	unsigned int x, y, size;
+	unsigned int x, y;
 	char *new_str;
+	unsigned int size = strlen(str);
 
-	/*allocates memory*/
-	str = malloc(sizeof(char) * (size));
+	/*allocates memory of new string*/
+	new_str = malloc(sizeof(char) * (size + 1));
 
-	if (str == NULL)
+	if (new_str == NULL)
 	{
 		return (NULL);
 	}
@@ -24,12 +26,10 @@ char *_strdup(char *str)
 	/*makes a copy of a string*/
 	for (x = 0, y = 0; x < size; x++, y++)
 	{
-		str[x] = new_str[y];
+		new_str[x] = str[y];
 	}
-		
-	str[size] = '\0';
+
 	new_str[size] = '\0';
 
-	free(new_str);
 	return (new_str);
 }
