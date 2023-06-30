214593d8a7eee608cfd8f270aa72c3be8380c4d4
michaellgans <michaellgans@hotmail.com>
2023-06-30 12:00:48 -0700

    test

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - sticks two strings together
+ * @s1: first string
+ * @s2: second string
+ * @n: where to start second string
+ * Return: new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	/* Define Variables */
+	char *new_string;
+	unsigned int length;
+	/* initialize value */
+	length = strlen(s1) + n;
+	/* If a string is NULL, make it empty. */
+	if (s1 == NULL)
+	{
+		s1 = ("");
+	}
+	if (s2 == NULL)
+	{
+		s2 = ("");
+	}
+	/* Allocate Memory */
+	new_string = malloc(sizeof(char) * (length + 1));
+	if (new_string == NULL)
+	{
+		return (NULL);
+	}
+	/* Concat Strings */
+	strncpy(new_string, s1, strlen(s1));
+	strncat(new_string, s2, n);
+	new_string[length] = '\0';
+
+	return (new_string);
+}
