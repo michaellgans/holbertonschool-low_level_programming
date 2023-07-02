a8dc47181c9500382eb77c21a8e58ac1445ccbd8
michaellgans <michaellgans@hotmail.com>
2023-07-02 16:11:08 -0700

    needed to store memory

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - who dis?
@@ -17,11 +18,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 	/* assign memory to new dog */
 	new_dog_who_dis = malloc(sizeof(dog_t));
 	if (new_dog_who_dis == NULL)
+	{
 		return (NULL);
+		free(new_dog_who_dis);
+	}
 	/* link new dog to data points */
-	new_dog_who_dis->name = name;
+	new_dog_who_dis->name = malloc(strlen(name) + 1);
+	if (new_dog_who_dis->name == NULL)
+	{
+		free(new_dog_who_dis);
+		return (NULL);
+	}
+	strcpy(new_dog_who_dis->name, name);
 	new_dog_who_dis->age = age;
-	new_dog_who_dis->owner = owner;
-
+	new_dog_who_dis->owner = malloc(strlen(owner) + 1);
+	if (new_dog_who_dis->owner == NULL)
+	{
+		free(new_dog_who_dis);
+		return (NULL);
+	}
+	strcpy(new_dog_who_dis->owner, owner);
 	return (new_dog_who_dis);
 }
