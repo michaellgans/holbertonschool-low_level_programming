6e1c51d5d03ef0aa0d85e7ac250594dc346d002f
michaellgans <michaellgans@hotmail.com>
2023-07-02 12:35:30 -0700

    test

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - info about a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: I'm not sure what this is going to do yet!
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
