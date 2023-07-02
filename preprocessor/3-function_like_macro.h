24e727ebe4b789f178a5d579d3c4826b4b8d1e70
michaellgans <michaellgans@hotmail.com>
2023-07-01 19:49:21 -0700

    SHORT HAND???

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,24 +1,6 @@
-#ifndef ABS_H
-#define ABS_H
+#ifndef ABS
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
 #include <stdio.h>
-
-/**
- * ABS - computes absolute value
- * @x: number to be computed
- * Return: 0
- */
-
-int ABS(int x)
-{
-	if (x <= 0)
-	{
-		x = (x * -1);
-	}
-	else
-	{
-		x = (x * 1);
-	}
-	return (x);
-}
 
 #endif
