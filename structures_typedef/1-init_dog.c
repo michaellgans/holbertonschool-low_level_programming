817442cabfa5522c7f8299234bcb3f3be29eee40
michaellgans <michaellgans@hotmail.com>
2023-07-02 13:41:10 -0700

    can return without returning anything

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * init_dog - initializes variables
@@ -13,7 +14,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		printf("OK\n");
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
