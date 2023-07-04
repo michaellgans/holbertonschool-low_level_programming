937c25434ea508b23ccbe5f30912630987d59325
michaellgans <michaellgans@hotmail.com>
2023-07-03 18:10:40 -0700

    test for null

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_name - prints name
@@ -9,5 +10,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		exit(0);
 	f(name);
 }
