7cc01a60d7c09c6e7c65bf666661f060b4a370be
michaellgans <michaellgans@hotmail.com>
2023-07-04 09:34:35 -0700

    need to remember that this function already exists

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * int_index - searches for integers
+ * @array: where to search
+ * @size: of array
+ * @cmp: pointer to int_index
+ * Return: 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+	/* if size <= 0, return -1 */
+	if (size <= 0)
+		return (-1);
+	/* if int_index is true, give that int, otherwise -1 */
+	for (x = 0; x < size; x++)
+	{
+		if (cmp(array[x]))
+			return (x);
+	}
+	return (-1);
+}
