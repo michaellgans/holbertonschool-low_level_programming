fadadf6d48cbfb03e6cce4cd6ecc10c89586879d
michaellgans <michaellgans@hotmail.com>
2023-07-04 09:36:03 -0700

    check for NULL

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -16,6 +16,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	/* if size <= 0, return -1 */
 	if (size <= 0)
 		return (-1);
+	if (array == NULL || cmp == NULL)
+		return (-1);
 	/* if int_index is true, give that int, otherwise -1 */
 	for (x = 0; x < size; x++)
 	{
