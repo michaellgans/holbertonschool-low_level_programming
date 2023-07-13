3724a8ac7cd925b7043d7600bf10fa600e2cf2a3
michaellgans <michaellgans@hotmail.com>
2023-07-13 08:54:06 -0700

    Task 0 updated

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints the list contained in nodes
+ * @h: head?
+ * Return: size_t
+ */
+
+size_t print_list(const list_t *h)
+{
+	/* Defining Variables */
+	size_t size = 0;
+	const list_t *YouAreHere = h;
+	int x;
+	/* Move through the list */
+	for (x = 0; YouAreHere != NULL; x++)
+	{
+		/* If the string is NULL, print [0] (nil) */
+		if (YouAreHere->str == NULL)
+			printf("[0] (nill)\n");
+		else
+			printf("[%d] %s\n", YouAreHere->len, YouAreHere->str);
+		size++;
+		YouAreHere = YouAreHere->next;
+	}
+	return (size);
+}
