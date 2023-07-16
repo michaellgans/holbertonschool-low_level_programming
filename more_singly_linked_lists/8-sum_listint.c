d99aad7ce5380982cb5ec371a99d2e822452f977
michaellgans <michaellgans@hotmail.com>
2023-07-15 20:26:02 -0700

    Created 8-sum_listint.c

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum the data in the list
+ * @head: pointer to head
+ * Return: sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	/* set current pointer to head and define sum */
+	int sum = 0;
+	listint_t *current = head;
+
+	/* Edge Case */
+	if (current->next == NULL)
+		return (0);
+
+	/* Go through list */
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (sum);
+}
