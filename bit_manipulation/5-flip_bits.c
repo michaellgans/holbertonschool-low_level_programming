009a4178df772e036967e7e0fb64e54e655d10e5
michaellgans <michaellgans@hotmail.com>
2023-07-17 12:47:10 -0700

    task 5

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the num of bits needed to be flipped
+ * to get from n to m
+ * @n: first location
+ * @m: second location
+ * Return: unsigned int
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int x, count = 0;
+
+	for (x = 0; x < (sizeof(n) * 8); x++)
+	{
+		if (((n >> x) & 1) != ((m >> i) & 1))
+			count++;
+	}
+	return (count);
+}
