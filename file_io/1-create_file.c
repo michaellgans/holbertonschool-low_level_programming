2d55e056497b9b8463ad553a695e76ca713fd71c
michaellgans <michaellgans@hotmail.com>
2023-07-17 18:14:09 -0700

    Test

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,16 +10,24 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	ssize_t bytesWritten;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
+	if (text_content != NULL)
+	{
+		bytesWritten = write(fd, text_content, strlen(text_content));
+		if (bytesWritten == -1)
+		{
+			return (-1);
+		}
+	}
 	close(fd);
 	return (1);
 }
