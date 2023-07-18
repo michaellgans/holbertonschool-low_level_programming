9630d11c167aaee770dd7594c35fe22d993884b1
michaellgans <michaellgans@hotmail.com>
2023-07-17 17:09:12 -0700

    Starting task 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a file and prints it to standard output
+ * @filename: points to file name
+ * @letters: number of letters to print
+ * Return: actual number of letters printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	/* Declare Variables */
+	int fd;
+	char *buffer;
+	ssize_t bytesRead, bytesWritten;
+
+	/* no filename */
+	if (filename == NULL)
+		return (0);
+	/* Open a file */
+	fd = open(filename, O_RDONLY);
+	/* Can't be opened? */
+	if (fd == -1)
+		return (0);
+	/* Allocate Memory */
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
+	if (lseek(fd, 0, SEEK_SET) == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	/* Read a file */
+	bytesRead = read(fd, buffer, letters);
+	/* Can't be read? */
+	if (bytesRead == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	/* Write to stdout */
+	bytesWritten = write(1, buffer, bytesRead);
+	free(buffer);
+	close(fd);
+	return (bytesWritten);
+}
