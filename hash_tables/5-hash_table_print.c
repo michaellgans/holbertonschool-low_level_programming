7cb0af6facbd3254afc02cf1b8d8c9ca7f1006b9
michaellgans <michaellgans@hotmail.com>
2023-07-24 17:31:26 -0700

    COMMA

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -9,6 +9,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int x; /* iterator */
 	hash_node_t *current; /* you are here */
+	int flag = 1; /* comma or no comma */
 
 	if (ht == NULL) /* if ht is empty, don't print */
 		return;
@@ -20,10 +21,12 @@ void hash_table_print(const hash_table_t *ht)
 		current = ht->array[x]; /* start at first index */
 		while (current != NULL)
 		{
-			printf("'%s': '%s'", current->key, current->value);
-			current = current->next; /* traverse */
-			if (current != NULL)
+			if (flag == 0)
 				printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+
+			flag = 0; /* next one gets a comma */
+			current = current->next; /* traverse */
 		}
 	}
 	printf("}\n");
